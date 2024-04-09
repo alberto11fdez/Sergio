0aff28dcf646fdf56e2c1141557c13287ef9e873
Serranito09 <alvaroserrano0407@gmail.com>
2024-04-09 15:48:11 +0200

    Add mongoose package to api/package.json and update api/node_modules/.package-lock.json

--- api/app.js
+++ api/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mongooseRouter = require('./data/mongoose');
 
 var app = express();
 
@@ -13,6 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use('/mongoose',mongooseRouter);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
