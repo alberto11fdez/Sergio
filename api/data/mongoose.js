f7644437d087551f69c0675e4db31fcb6508c01b
Serranito09 <alvaroserrano0407@gmail.com>
2024-04-10 10:57:46 +0200

    Refactor mongoose schema field names in api/data/mongoose.js

--- api/data/mongoose.js
+++ api/data/mongoose.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 var mongoose = require("mongoose");
-const uri = "mongodb+srv://admin:hTVNa7w7kjJZdg@cluster0.3wkekj7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://admin:hTVNa7w7kjJZdg@cluster0.3wkekj7.mongodb.net/SUB-MAS";
 
 mongoose.connect(uri)
 .then(()=>{
@@ -12,40 +12,5 @@ mongoose.connect(uri)
     console.log("Error al conectar a mongodb");
 });
 
-const CommentSchema =new mongoose.Schema({
-    _id:String,
-    nombre:String,
-    apellidos:String,
-    correo:String,
-    contraseña:String,
-},{'collection':'Users'});
-
-const Comment = mongoose.model('SUB-MAS',CommentSchema,'Users');
-
-router.get("/", async (req,res)=>{
-    try{
-        const comments = await Comment.find({'_id':req.query.id_post});
-        if(comments){
-            res.send(comments);
-        }
-        else{
-            res.send({});
-        }
-    }catch(error){
-        console.log(error);
-        res.status(400).json({error:error.message});
-    }
-})
-
-router.post("/", async(req,res)=>{
-    try{
-        const new_comment= new Comment(req.body);
-        const savedComment = await new_comment.save();
-        res.status(201).send(savedComment);
-    }catch(error){
-        console.log(error);
-        res.status(400).json({error:error.message}); 
-    }
-});
 
 module.exports=router;
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require("mongoose");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const UsersSchema =new mongoose.Schema({
+    
+  nombre:String,
+  apellidos:String,
+  correo:String,
+  contraseña:String,
+},{'collection':'Users'});
 
+const Users = mongoose.model('SUB-MAS',UsersSchema,'Users');
+
+router.get("/", async (req,res)=>{
+  try{
+      const comments = await Users.find({'nombre':req.query.nombre});
+      if(comments){
+          res.send(comments);
+      }
+      else{
+          res.send({});
+      }
+  }catch(error){
+      console.log(error);
+      res.status(400).json({error:error.message});
+  }
+})
+
+router.post("/", async(req,res)=>{
+  try{
+      const new_comment= new Comment(req.body);
+      const savedComment = await new_comment.save();
+      res.status(201).send(savedComment);
+  }catch(error){
+      console.log(error);
+      res.status(400).json({error:error.message}); 
+  }
+});
 module.exports = router;
